a44a89b70be83e0bb0c485f7005ec7163cba9feb
Catherine Cook <cookcm@us.ibm.com>
2022-01-30 14:31:29 -0600

    Add store to local storage

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import  {configureStore}  from "./store";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import { configureStore } from "./store";
 import "./index.scss";
 
 import Page from "./pages/Page";
+
+const store = configureStore();
+const persistor = persistStore(store);
 
 const App = () => (
   <div className="app">
@@ -13,8 +18,10 @@ const App = () => (
 );
 
 render(
-  <Provider store={configureStore()}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
